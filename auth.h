3bee01a9e020ece6ca3f8d5bfe6aa001054e9b9f
Yuri <yuri.rage@gmailcom>
2021-08-11 12:24:43 -0500

    re-added auth.h

---
+++ auth.h
@@ -0,0 +1,14 @@
+/*********************************************************************************
+ * auth.h - WiFI and OpenWeatherMap info for WebTachometer.ino
+ * 
+ * Enter your WiFi SSID and password below
+ * 
+ * -- Yuri - Aug 2021
+*********************************************************************************/
+#ifndef AUTH_H
+#define AUTH_H
+
+const char* ssid     = "<SSID>";
+const char* password = "<Your Password>";
+
+#endif // AUTH_H
